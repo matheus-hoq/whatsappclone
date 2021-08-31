186864873aad89cc0da80827e49b55522a9bfdab
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-08-31 10:38:23 -0300

    material icons UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@
 import React from 'react';
 import './App.css';
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 export default () => {
   return (
     <div className="app-window">
       <div className="sidebar">
-        ...
+
+        <header>
+          <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}}/>
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          ...
+        </div>
+        
+        <div className="chatlist">
+          ...
+        </div>
+
       </div>
       <div className="contentarea">
         ...
