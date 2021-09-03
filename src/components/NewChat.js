23a33cbab4b6982f63de503ca738a39f412fdb1c
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-09-03 18:10:36 -0300

    inicio new chat

--- src/components/NewChat.js
+++ src/components/NewChat.js
@@ -1,10 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, {useState} from 'react';
 import './NewChat.css';
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 export default () => {
+    const [list, setList] = useState([
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Matheus Henrique'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Matheus Henrique'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Matheus Henrique'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Matheus Henrique'}
+    ]);
+
+
     return (
         <div className="newChat">
             <div className="newChat--head">
@@ -14,7 +22,11 @@ export default () => {
                 <div className="newChat--headtitle">Nova Conversa</div>
             </div>
             <div className="newChat--list">
+                {list.map((item, key)=>{
+                    <div className="newChat--item">
 
+                    </div>
+                })}
             </div>
         </div>
     )
