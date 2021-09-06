c6b0211f501ed94e14ad8358832765b202285ae0
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-09-06 12:27:23 -0300

    continuação api + login

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,5 @@
-import firebase from 'firabse/app';
+/* eslint-disable import/no-anonymous-default-export */
+import firebase from 'firebase/app';
 import 'firebase/firebase-auth';
 import 'firebase/firebase-firestore';
 
@@ -6,3 +7,11 @@ import firebaseConfig from './firebaseConfig';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore();
+
+export default {
+    fbPopup:async () => {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        let result = await firebaseApp.auth().signInWithPopup(provider);
+        return result;
+    }
+};
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import Api from '../Api';
 import './Login.css';
 
-export default () => {
-    const handleFacebookLogin = () => {
-        
+export default ({onReceive}) => {
+    const handleFacebookLogin = async () => {
+        let result = await Api.fbPopup();
+        if(result) {
+            onReceive(result.user)
+        } else {
+            alert ("Erro!");
+        }
     }
 
 
